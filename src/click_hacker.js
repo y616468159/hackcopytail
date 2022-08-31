3eb8e603b05352acdc8a9125ca06567b7ef265fd
bin.yin <616468159@qq.com>
2022-08-31 14:03:44 +0800

    [CSDN] 关注博主即可阅读全文

--- src/click_hacker.js
+++ src/click_hacker.js
@@ -1,4 +1,14 @@
 (function () {
+    // CSDN 关注博主即可阅读全文
+    let nodeArr = document.getElementsByClassName('hide-article-box hide-article-pos text-center');
+    if (nodeArr.length === 1) {
+        nodeArr[0].remove();
+        let contentNode = document.getElementById('article_content');
+        contentNode.style.height = '';
+        contentNode.style.overflow = '';
+    }
+
+    // CSDN 免登录复制
     function doSome(nodeArr) {
         for(let item of nodeArr) {
             item.style.setProperty('user-select', 'text')
@@ -7,6 +17,7 @@
     doSome(document.querySelectorAll('#content_views pre'))
     doSome(document.querySelectorAll('#content_views pre code'));
 
+    // CSDN 可编辑
     chrome.storage.local.get('editableCSDN', (result) => {
         if (result.editableCSDN) {
             document.body.contentEditable='true';
